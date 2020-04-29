f33954e67639c57428f0053c4fe2766ea0a5623b
charlez136 <61160821+charlez136@users.noreply.github.com>
2020-04-29 14:01:46 +0300

    Adding user and adding assets

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,13 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AuthenticationService } from '../_services/authentication.service';
+import { User } from '../_models';
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.scss']
 })
 export class UserComponent implements OnInit {
-
-  constructor() { }
+  currentUser: User;
+  constructor(private authenticationService: AuthenticationService) {
+    this.currentUser = this.authenticationService.currentUserValue;
+   }
 
   ngOnInit(): void {
   }
